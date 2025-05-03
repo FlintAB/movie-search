c054f7fe7f022c1840fc9cb0e2d55021252d514d
Aleksey Baranovskiy <lari.bro@yandex.ru>
2025-05-03 19:06:36 +0300

    add/Debounce for SearchForm component

--- src/components/searchForm/SearchForm.tsx
+++ src/components/searchForm/SearchForm.tsx
@@ -1,24 +1,29 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { searchMovies } from "../../services/slices/searchSlice";
 import { RootState, useAppDispatch } from "../../services/store";
 import { useSelector } from "react-redux";
-
-
-
+import { useDebounce } from "../../hooks/useDebounce";
 
 export const SearchForm: FC = () => {
    const [query, setQuery] = useState('');
+   const debouncedQuery = useDebounce(query, 1000);
    const {status} = useSelector((state: RootState) => state.search)
    const dispatch = useAppDispatch();
 
-   function handleSubmit (e: React.FormEvent) {
+   useEffect(() => {
+      if (debouncedQuery.trim()) {
+         dispatch(searchMovies(debouncedQuery));
+      }
+   }, [debouncedQuery, dispatch]);
+
+   function handleSubmit(e: React.FormEvent) {
       e.preventDefault();
       if (query.trim()) {
-         console.log(query);
          dispatch(searchMovies(query));
       }
    };
-   function handleChange (e: React.ChangeEvent<HTMLInputElement>){
+
+   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
       setQuery(e.target.value);
    }
 
